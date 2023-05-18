ea282b2e277a4b51f94a2e8a51709e8db5471631
Rana1017 <toprana212@gmail.com>
2023-05-18 22:33:25 +0545

    Added homepagePicture and css

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,22 +5,32 @@ import { TbArrowRightSquare } from "react-icons/tb";
 const Home = () => {
   const { homeImageUrl, aboutMe } = data;
   return (
-    <div>
-      <div>
-        <div>
-          <h2>I'm a Front End Developer</h2>
-          <p>{aboutMe}</p>
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 "
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-6xl text-white">
+            I'm a Front End Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md"> {aboutMe}</p>
           <div>
-            <button>
+            <button className="text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500">
               Portfolio
-              <span>
+              <span className="text-2xl">
                 <TbArrowRightSquare />
               </span>
             </button>
           </div>
         </div>
+
         <div>
-          <img src={homeImageUrl} alt="photos of mine" />
+          <img
+            src={homeImageUrl}
+            alt="profile of mine"
+            className="rounded-2xl mx-auto w-2/3 md:w-full bg-slate-400"
+          />
         </div>
       </div>
     </div>
