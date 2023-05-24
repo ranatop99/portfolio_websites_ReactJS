14b581094bf6a0216f2d82a6c4d132ce74e8cc67
ranatop99 <toprana212@gmail.com>
2023-05-24 10:06:30 +0545

    Work in Progress

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Contact
           </p>
-          <p className="py-2">Get in touch with me:</p>
+          <p className="py-6">Get in touch with me:</p>
         </div>
         <div className="flex justify-center items-center">
           <form
